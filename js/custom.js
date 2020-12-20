0d6770b0ae2f7e7412cf6899ea72dcc3e2f64de7
vitalii-dp <vitaliil87@gmail.com>
2020-12-20 12:49:54 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -21,68 +21,15 @@
           }
     });
 
-
-    // HOME SLIDER & COURSES & CLIENTS
-    $('.home-slider').owlCarousel({
-      animateOut: 'fadeOut',
-      items:1,
-      loop:true,
-      dots:false,
-      autoplayHoverPause: false,
-      autoplay: true,
-      smartSpeed: 1000,
-    })
-
-    $('.owl-courses').owlCarousel({
-      animateOut: 'fadeOut',
-      loop: true,
-      autoplayHoverPause: false,
-      autoplay: true,
-      smartSpeed: 1000,
-      dots: false,
-      nav:true,
-      navText: [
-          '<i class="fa fa-angle-left"></i>',
-          '<i class="fa fa-angle-right"></i>'
-      ],
-      responsiveClass: true,
-      responsive: {
-        0: {
-          items: 1,
-        },
-        1000: {
-          items: 3,
-        }
-      }
-    });
-
-    $('.owl-client').owlCarousel({
-      animateOut: 'fadeOut',
-      loop: true,
-      autoplayHoverPause: false,
-      autoplay: true,
-      smartSpeed: 1000,
-      responsiveClass: true,
-      responsive: {
-        0: {
-          items: 1,
-        },
-        1000: {
-          items: 3,
-        }
-      }
-    });
-
-
     // SMOOTHSCROLL
     $(function() {
-      $('.custom-navbar a, #home a').on('click', function(event) {
+      $('.custom-navbar a, #home a, #services a').on('click', function(event) {
         var $anchor = $(this);
           $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top - 49
           }, 1000);
             event.preventDefault();
       });
-    });  
+    }); 
 
 })(jQuery);
